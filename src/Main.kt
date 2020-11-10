bf76d8c7bb6afc5b21710de6f45d082df94c95f2
compandcode <66060875+vncomp@users.noreply.github.com>
2020-11-10 17:48:12 +0000

    Kotlin Datatypes.

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,19 @@
 fun main() {
     println("Hello World")
 
-    var x: Int = 32
-    print(x)
+    val wholeNumber = 3 //Int.
+    val bigNumber = 3L //Long.
+    val preciseDecimal = 3.33 //Double.
+    val decimal = 3.33f //Float.
+    val character = 'c'
+    //Booleans.
+    val isItRaining = true
+    val isItTimeToSleep = false
+
+
+
+    var x: Int = 32 //Use 'val' for CONSTANTS. var x = 32
+    println("The Value of x is: $x")
+    x = 10000
+    println("x has changed to: $x")
 }
