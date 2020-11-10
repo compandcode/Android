17787838bde40a737ec7481829e3ed962bc7e6c9
compandcode <66060875+vncomp@users.noreply.github.com>
2020-11-10 18:13:47 +0000

    Finished 'IF Conditions' 30:00

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,39 @@
 fun main() {
-    println("Hello World")
+//    println("Hello World")
+//
+//    val wholeNumber = 3 //Int.
+//    val bigNumber = 3L //Long.
+//    val preciseDecimal = 3.33 //Double.
+//    val decimal = 3.33f //Float.
+//    val character = 'c'
+//    //Booleans.
+//    val isItRaining = true
+//    val isItTimeToSleep = false
+//
+//    println(isItRaining || isItTimeToSleep) //&& , ||
+//
+//    var x: Int = 32 //Use 'val' for CONSTANTS. var x = 32
+//    println("The Value of x is: $x")
+//    x = 10000
+//    println("x has changed to: $x")
+//
+//    var sum = 10+3 // +-*/
+//
+//    //Important for Kotlin.
+//    println(3/4)
+//    println(3f/4f)
 
-    val wholeNumber = 3 //Int.
-    val bigNumber = 3L //Long.
-    val preciseDecimal = 3.33 //Double.
-    val decimal = 3.33f //Float.
-    val character = 'c'
-    //Booleans.
-    val isItRaining = true
-    val isItTimeToSleep = false
+    //Strings
+    val string = "KING KING KING"
+    println(string.toLowerCase())
 
-    println(isItRaining || isItTimeToSleep) //&& , ||
+    val x = 5
 
-    var x: Int = 32 //Use 'val' for CONSTANTS. var x = 32
-    println("The Value of x is: $x")
-    x = 10000
-    println("x has changed to: $x")
+    if (x == 6) {
+        println("Yes")
+    } else {
+        println("No")
+    }
 
-    var sum = 10+3 // +-*/
 
-    //Important for Kotlin.
-    println(3/4)
-    println(3f/4f)
 }
