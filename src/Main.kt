6f790d9652c201bd945aeea84b5d695333678e59
compandcode <66060875+vncomp@users.noreply.github.com>
2020-11-10 17:51:37 +0000

    Completed Arithmetic Section.

--- src/Main.kt
+++ src/Main.kt
@@ -10,10 +10,14 @@ fun main() {
     val isItRaining = true
     val isItTimeToSleep = false
 
-
-
     var x: Int = 32 //Use 'val' for CONSTANTS. var x = 32
     println("The Value of x is: $x")
     x = 10000
     println("x has changed to: $x")
+
+    var sum = 10+3 // +-*/
+
+    //Important for Kotlin.
+    println(3/4)
+    println(3f/4f)
 }
